7b6fb55fa4108e5b117056701957f23b59f6617f
Akio Kane <aakocherga@mail.ru>
2026-01-03 13:58:00 +0700

    create base frontend structure

---
+++ client/user_pages/src/main.tsx
@@ -0,0 +1,16 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import routes from "./routes/routes";
+import "./index.css";
+import { AppProvider } from "./context/AppContext";
+
+const router = createBrowserRouter(routes);
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <AppProvider>
+      <RouterProvider router={router} />
+    </AppProvider>
+  </StrictMode>
+);
---
+++ client/user_pages/src/routes/App.tsx
@@ -0,0 +1,12 @@
+import type React from 'react';
+import './App.module.css'
+
+const App: React.FC = () => {
+  return (
+    <>
+      
+    </>
+  );
+};
+
+export default App;
